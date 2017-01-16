fb846466f819587b6a35b0197c77ca4f082243d4
doanc <doanc@DESKTOP-67UEKDG>
2017-01-16 23:39:45 +0700

    init angularjs

---
+++ SuiteSolution/SuiteSolution.WebAPI/wwwroot/app.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('suiteApp', [
+        // Angular modules 
+        'ngRoute',
+        'blockUI', 'ngSanitize', 'ui.bootstrap', 'ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.pagination', 'ui.grid.selection', 'ui.grid.exporter'
+        // Custom modules 
+        // 3rd Party Modules
+    ]);
+
+    app.config(function ($httpProvider) {
+        $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+        $httpProvider.defaults.withCredentials = true;
+    });
+
+    app.config(function (blockUIConfig) {
+
+        // Change the default overlay message
+        blockUIConfig.message = 'Please stop clicking!';
+        // Change the default delay to 100ms before the blocking is visible
+        blockUIConfig.delay = 100;
+        // Disable automatically blocking of the user interface
+        blockUIConfig.autoBlock = false;
+
+    });
+
+
+
+})();
