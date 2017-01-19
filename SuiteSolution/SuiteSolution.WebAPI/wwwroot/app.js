d1079e8de030552e1a410d1e21bd9c513f85a3b0
doanc <doanc@DESKTOP-67UEKDG>
2017-01-20 00:01:24 +0700

    xxxxxxxxx

--- SuiteSolution/SuiteSolution.WebAPI/wwwroot/app.js
+++ SuiteSolution/SuiteSolution.WebAPI/wwwroot/app.js
@@ -1,96 +1,91 @@
 ﻿(function () {
     'use strict';
+        var app = angular.module('suiteApp', [
+            // Angular modules 
+            'ngRoute',
+            'blockUI', 'ngSanitize', 'ui.bootstrap', 'ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.pagination', 'ui.grid.selection', 'ui.grid.exporter'
+            // Custom modules 
+            // 3rd Party Modules
+        ]);
 
-    var app = angular.module('suiteApp', [
-        // Angular modules 
-        'ngRoute',
-        'blockUI', 'ngSanitize', 'ui.bootstrap', 'ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.pagination', 'ui.grid.selection', 'ui.grid.exporter'
-        // Custom modules 
-        // 3rd Party Modules
-    ]);
+        app.config(['$httpProvider', function ($httpProvider) {
+            $httpProvider.interceptors.push('xmlHttpInteceptor');
+            if (!$httpProvider.defaults.headers.get) {
+                $httpProvider.defaults.headers.get = {};
+            }
+            $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+            $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+        }]);
+        function errorHandler(status, message) {
+            var scope = angular.element($('html')).scope();
+            scope.errorHandler(status, message);
+        };
+        app.config(function (blockUIConfig) {
 
-    app.config(['$httpProvider', function ($httpProvider) {
-        $httpProvider.interceptors.push('xmlHttpInteceptor');
-        if (!$httpProvider.defaults.headers.get) {
-            $httpProvider.defaults.headers.get = {};
-        }
-        $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
-        $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
-    }]);
-    function errorHandler(status, message) {
-        var scope = angular.element($('html')).scope();
-        scope.errorHandler(status, message);
-    };
-    app.config(function (blockUIConfig) {
+            // Change the default overlay message
+            blockUIConfig.message = 'Please stop clicking!';
+            // Change the default delay to 100ms before the blocking is visible
+            blockUIConfig.delay = 100;
+            // Disable automatically blocking of the user interface
+            blockUIConfig.autoBlock = false;
 
-        // Change the default overlay message
-        blockUIConfig.message = 'Please stop clicking!';
-        // Change the default delay to 100ms before the blocking is visible
-        blockUIConfig.delay = 100;
-        // Disable automatically blocking of the user interface
-        blockUIConfig.autoBlock = false;
+        });
 
-    });
+        //app.config(['$routeProvider', function ($routeProvider) {
+        //    $routeProvider
+        //    .when('/', {
+        //        templateUrl: 'index.htm', controller: 'indexController'
+        //    })
+        //    .when('/:section/:tree', {
+        //        templateUrl: function (rp) { return 'views/' + rp.section + '/' + rp.tree + '.html'; },
+        //        resolve: {
 
-    //app.config(['$routeProvider', function ($routeProvider) {
-    //    $routeProvider
-    //    .when('/', {
-    //        templateUrl: 'index.htm', controller: 'indexController'
-    //    })
-    //    .when('/:section/:tree', {
-    //        templateUrl: function (rp) { return 'views/' + rp.section + '/' + rp.tree + '.html'; },
-    //        resolve: {
+        //            load: ['$q', '$rootScope', '$location', function ($q, $rootScope, $location) {
 
-    //            load: ['$q', '$rootScope', '$location', function ($q, $rootScope, $location) {
+        //                var path = $location.path();
+        //                var parsePath = path.split("/");
+        //                var parentPath = parsePath[1];
+        //                var controllerName = parsePath[2];
 
-    //                var path = $location.path();
-    //                var parsePath = path.split("/");
-    //                var parentPath = parsePath[1];
-    //                var controllerName = parsePath[2];
+        //                var loadController = "controllers/" + controllerName + "Controller";
 
-    //                var loadController = "controllers/" + controllerName + "Controller";
+        //                var deferred = $q.defer();
+        //                require([loadController], function () {
+        //                    $rootScope.$apply(function () {
+        //                        deferred.resolve();
+        //                    });
+        //                });
+        //                return deferred.promise;
+        //            }]
+        //        }
+        //    })
+        //    .when("/:section/:tree/:id", {
+        //        templateUrl: function (rp) { return 'views/' + rp.section + '/' + rp.tree + '.html'; },
+        //        resolve: {
 
-    //                var deferred = $q.defer();
-    //                require([loadController], function () {
-    //                    $rootScope.$apply(function () {
-    //                        deferred.resolve();
-    //                    });
-    //                });
-    //                return deferred.promise;
-    //            }]
-    //        }
-    //    })
-    //    .when("/:section/:tree/:id", {
-    //        templateUrl: function (rp) { return 'views/' + rp.section + '/' + rp.tree + '.html'; },
-    //        resolve: {
+        //            load: ['$q', '$rootScope', '$location', function ($q, $rootScope, $location) {
 
-    //            load: ['$q', '$rootScope', '$location', function ($q, $rootScope, $location) {
+        //                var path = $location.path();
+        //                var parsePath = path.split("/");
+        //                var parentPath = parsePath[1];
+        //                var controllerName = parsePath[2];
 
-    //                var path = $location.path();
-    //                var parsePath = path.split("/");
-    //                var parentPath = parsePath[1];
-    //                var controllerName = parsePath[2];
+        //                var loadController = "controllers/" + controllerName + "Controller";
 
-    //                var loadController = "controllers/" + controllerName + "Controller";
+        //                var deferred = $q.defer();
+        //                require([loadController], function () {
+        //                    $rootScope.$apply(function () {
+        //                        deferred.resolve();
+        //                    });
+        //                });
+        //                return deferred.promise;
+        //            }]
+        //        }
+        //    })
+        //    .otherwise({
+        //        redirectTo: '/'
+        //    });
 
-    //                var deferred = $q.defer();
-    //                require([loadController], function () {
-    //                    $rootScope.$apply(function () {
-    //                        deferred.resolve();
-    //                    });
-    //                });
-    //                return deferred.promise;
-    //            }]
-    //        }
-    //    })
-    //    .otherwise({
-    //        redirectTo: '/'
-    //    });
-
-    //}]);
-
-
-
-
-
+        //}]);
+   
 })();
---
+++ SuiteSolution/SuiteSolution.WebAPI/wwwroot/controllers/indexController.js
@@ -0,0 +1,86 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('suiteApp')
+        .controller('indexController', indexController);
+
+    indexController.$inject = ['$scope', '$rootScope', '$http', '$location', 'blockUI'];
+
+    function indexController($scope, $rootScope, $http, $location, blockUI) {
+        $scope.$on('$routeChangeStart', function (scope, next, current) {
+            if ($rootScope.IsloggedIn == true) {
+                $scope.authenicateUser($location.path(), $scope.authenicateUserComplete, $scope.authenicateUserError);
+            }
+        });
+
+        $scope.$on('$routeChangeSuccess', function (scope, next, current) {
+
+            setTimeout(function () {
+                if ($scope.isCollapsed == true) {
+                    set95PercentWidth();
+                }
+            }, 1000);
+
+
+        });
+
+
+        $scope.initializeController = function () {
+            $rootScope.displayContent = false;
+            if ($location.path() != "") {
+                $scope.initializeApplication($scope.initializeApplicationComplete, $scope.initializeApplicationError);
+            }
+        }
+
+        $scope.initializeApplicationComplete = function (response) {
+            $rootScope.MenuItems = response.MenuItems;
+            $rootScope.displayContent = true;
+            $rootScope.IsloggedIn = true;
+        }
+
+        $scope.initializeApplication = function (successFunction, errorFunction) {
+            blockUI.start();
+            $scope.AjaxGet("/api/main/InitializeApplication", successFunction, errorFunction);
+            blockUI.stop();
+        };
+
+        $scope.authenicateUser = function (route, successFunction, errorFunction) {
+            var authenication = new Object();
+            authenication.route = route;
+            $scope.AjaxGetWithData(authenication, "/api/main/AuthenicateUser", successFunction, errorFunction);
+        };
+
+        $scope.authenicateUserComplete = function (response) {
+
+            //if (response.IsAuthenicated == false)
+            //    window.location = "/index.html";
+        }
+
+        $scope.authenicateUserError = function (response) {
+            alert("ERROR= " + response.IsAuthenicated);
+        }
+
+        $scope.AjaxGet = function (route, successFunction, errorFunction) {
+            setTimeout(function () {
+                $http({ method: 'GET', url: route }).success(function (response, status, headers, config) {
+                    successFunction(response, status);
+                }).error(function (response) {
+                    errorFunction(response);
+                });
+            }, 1);
+
+        }
+
+        $scope.AjaxGetWithData = function (data, route, successFunction, errorFunction) {
+            setTimeout(function () {
+                $http({ method: 'GET', url: route, params: data }).success(function (response, status, headers, config) {
+                    successFunction(response, status);
+                }).error(function (response) {
+                    errorFunction(response);
+                });
+            }, 1);
+
+        }
+    }
+})();
